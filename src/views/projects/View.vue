88fcd8cea669078b59e57e91a86cd8c15ad13ec2
Matija Osojnik <matija1.osojnik@gmail.com>
2021-03-08 12:56:31 +0100

    Add Trendlines to Projects

--- src/views/projects/View.vue
+++ src/views/projects/View.vue
@@ -4,11 +4,83 @@
       <v-flex xs12 justify="center" align="center">
         <v-card class="mx-auto" v-if="project">
           <v-toolbar flat color="#617BE3" dark>
-            <v-toolbar-title>{{project.title}}</v-toolbar-title>
+            <v-toolbar-title>{{ project.title }}</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-              {{project.description}}
+            {{ project.description }}
           </v-card-text>
+          <v-row class="mx-2">
+            <v-col>
+              <v-card class="mt-4 mx-auto py-2">
+                <v-sheet
+                  class="v-sheet--offset mx-auto my-2"
+                  color="cyan"
+                  elevation="12"
+                  max-width="calc(100% - 32px)"
+                >
+                  <v-sparkline
+                    :value="value"
+                    :labels="labels"
+                    color="rgba(255, 255, 255, .7)"
+                    height="100"
+                    padding="24"
+                    stroke-linecap="round"
+                    smooth
+                    auto-draw
+                  ></v-sparkline>
+                </v-sheet>
+
+                <v-card-text class="pt-0">
+                  <div class="title font-weight-light mb-2">
+                    User Registrations
+                  </div>
+                  <div class="subheading font-weight-light grey--text">
+                    Last Campaign Performance
+                  </div>
+                  <v-divider class="my-2"></v-divider>
+                  <v-icon class="mr-2" small> mdi-clock </v-icon>
+                  <span class="caption grey--text font-weight-light"
+                    >last registration 26 minutes ago</span
+                  >
+                </v-card-text>
+              </v-card>
+            </v-col>
+            <v-col>
+              <v-card class="mt-4 mx-auto  py-2">
+                <v-sheet
+                  class="v-sheet--offset mx-auto my-2"
+                  color="cyan"
+                  elevation="12"
+                  max-width="calc(100% - 32px)"
+                >
+                  <v-sparkline
+                    :value="value"
+                    :labels="labels"
+                    color="rgba(255, 255, 255, .7)"
+                    height="100"
+                    padding="24"
+                    stroke-linecap="round"
+                    smooth
+                    auto-draw
+                  ></v-sparkline>
+                </v-sheet>
+
+                <v-card-text class="pt-0">
+                  <div class="title font-weight-light mb-2">
+                    User Registrations
+                  </div>
+                  <div class="subheading font-weight-light grey--text">
+                    Last Campaign Performance
+                  </div>
+                  <v-divider class="my-2"></v-divider>
+                  <v-icon class="mr-2" small> mdi-clock </v-icon>
+                  <span class="caption grey--text font-weight-light"
+                    >last registration 26 minutes ago</span
+                  >
+                </v-card-text>
+              </v-card> 
+            </v-col>
+          </v-row>
         </v-card>
       </v-flex>
     </v-layout>
@@ -23,18 +95,33 @@ export default {
     errors: [],
     menu: false,
     modal: false,
+    type: "trend",
+    value: [200, 675, 410, 390, 310, 460, 250, 240],
+    labels: [
+      "24:00",
+      "3:00",
+      "06:00",
+      "09:00",
+      "12:00",
+      "15:00",
+      "18:00",
+      "21:00",
+    ],
   }),
   mounted() {
-      this.loadProject();
+    this.loadProject();
   },
   methods: {
-    async loadProject() { 
+    async loadProject() {
       this.waitBeforeClick = true;
       try {
-      const id = this.$route.params.id
-      const response = await ProjectService.single(id, this.$store.state.token);
+        const id = this.$route.params.id;
+        const response = await ProjectService.single(
+          id,
+          this.$store.state.token
+        );
         if (response) {
-            this.project = response.data.item
+          this.project = response.data.item;
         }
       } catch (err) {
         setTimeout(() => (this.waitBeforeClick = false), 3000);
