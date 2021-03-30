6cb2dcc71241dae58ddeb84f7837fc3c630df02c
Matija Osojnik <matija1.osojnik@gmail.com>
2021-03-30 22:23:43 +0200

    Remove yarn.lock file

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 let baseurl = ''
 if(process.env.NODE_ENV === 'production'){
-    baseurl = 'https://checkyapi.herokuapp.com/api'
+    baseurl = 'https://checky-backend.herokuapp.com/api'
 }else {
     baseurl = 'http://localhost:5000/api'
 }
