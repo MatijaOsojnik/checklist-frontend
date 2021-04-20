d0a736629a44d7e6e06bd60b441de3d9c75833de
Matija Osojnik <matija1.osojnik@gmail.com>
2021-04-20 22:33:54 +0200

    Fix Stats page, Check if the user is an admin

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,7 @@
       <div>
         <v-list-item class="px-2" v-if="user">
           <v-list-item-avatar>
-            <v-img
-              src="./assets/luke-chesser-pJadQetzTkI-unsplash.jpg"
-            ></v-img>
+            <v-img src="./assets/luke-chesser-pJadQetzTkI-unsplash.jpg"></v-img>
           </v-list-item-avatar>
           <v-list-item-title
             >{{ user.name }} {{ user.surname }}</v-list-item-title
@@ -21,13 +19,34 @@
         <v-divider />
 
         <v-list expand nav>
-          <v-list-item v-for="item in items" :key="item.title" :to="{name: item.link}">
+          <v-list-item :to="{ name: items[0].link }">
             <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
+              <v-icon>{{ items[0].icon }}</v-icon>
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
+              <v-list-item-title>{{ items[0].title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item :to="{ name: items[1].link }">
+            <v-list-item-icon>
+              <v-icon>{{ items[1].icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ items[1].title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            v-if="admin"
+            :to="{ name: items[2].link }"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ items[2].icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ items[2].title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -62,26 +81,34 @@ export default {
 
   data: () => ({
     user: null,
+    admin: false,
     drawer: true,
     items: [
-      { title: "Projekti", icon: "mdi-folder-multiple", link: 'projects' },
-      { title: "Statistika", icon: "mdi-chart-line", link: 'user-view' },
-      { title: "Moj račun", icon: "mdi-account", link: 'admin-view' },
+      { title: "Projekti", icon: "mdi-folder-multiple", link: "projects" },
+      { title: "Statistika", icon: "mdi-chart-line", link: "user-view" },
+      { title: "Nadzorna plošča", icon: "mdi-account", link: "admin-view" },
     ],
     mini: true,
   }),
   created() {
-    this.loadUser()
+    this.loadUser();
   },
   methods: {
-      async loadUser() {
+    async loadUser() {
       if (this.$store.state.token) {
         const token = this.$store.state.token;
-        this.user = jwtDecode(token);
+        const user = jwtDecode(token)
+        this.user = user
         this.$store.dispatch("setUser", this.user);
+        console.log(this.user.roles)
+        
+        const isAdmin = this.user.roles.filter(role => role.name === 'admin' ? true : null)
+        if(isAdmin.length) {
+          this.admin = true
+        }
       }
     },
-  }
+  },
 };
 </script>
 
